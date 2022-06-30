e8af38159aac3361bdad91b940e87e324f651b21
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-06-30 12:16:01 -0300

    Iniciando formulário django emanipulando dados

---
+++ passagens/views.py
@@ -0,0 +1,14 @@
+from multiprocessing import context
+from django.shortcuts import render
+from passagens.forms import PassagemForms
+
+def index(request):
+    form = PassagemForms()
+    contexto = {'form':form}
+    return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {'form':form}
+        return render(request, 'minha_consulta.html', contexto)
