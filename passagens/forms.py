d0146b27490c5013cd946326494be3d82b9ca1a3
igor.rabelo <igor.rabelo@coopersystem.com.br>
2022-06-30 12:21:15 -0300

    Incluindo campo de data pesquisa no formulário

--- passagens/forms.py
+++ passagens/forms.py
@@ -1,8 +1,12 @@
 from django import forms
 from tempus_dominus.widgets import DatePicker
+from datetime import datetime
 
 class PassagemForms(forms.Form):
     origem = forms.CharField(label='Origem', max_length=100)
     destino = forms.CharField(label='Destino', max_length=100)
     data_ida = forms.DateField(label='Ida', widget=DatePicker())
     data_volta = forms.DateField(label='Volta', widget=DatePicker())
+    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
+    
+    
